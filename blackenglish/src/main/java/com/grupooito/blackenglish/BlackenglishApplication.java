fde5d95d367e268bc66b83b7bdee883eb24d0c73
Anderson Freires <dre.son36@gmail.com>
2021-12-12 07:49:05 -0200

    System for weighing the black and brown community

---
+++ blackenglish/src/main/java/com/grupooito/blackenglish/BlackenglishApplication.java
@@ -0,0 +1,13 @@
+package com.grupooito.blackenglish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackenglishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackenglishApplication.class, args);
+	}
+
+}
---
+++ blackenglish/src/test/java/com/grupooito/blackenglish/BlackenglishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupooito.blackenglish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackenglishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
